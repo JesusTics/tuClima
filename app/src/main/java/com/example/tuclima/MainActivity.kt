7ac53f0eb95d73e0dc6bf04d3bfd5b9209834a02
JesusTics <jesusantonio2000@hotmail.com>
2022-08-28 15:36:04 -0600

    Ya se consigue la temperatura de la ciudad

--- app/src/main/java/com/example/tuclima/MainActivity.kt
+++ app/src/main/java/com/example/tuclima/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.tuclima
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.android.volley.NetworkResponse
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +19,32 @@ class MainActivity : AppCompatActivity() {
 
         val lat=intent.getStringExtra("lat")
         var long=intent.getStringExtra("long")
-        Toast.makeText(this,lat+" "+long, Toast.LENGTH_LONG).show()
+
+
+        window.statusBarColor= Color.parseColor("#42B839")
+        getJsonData()
 
     }
+    private fun getJsonData()
+    {
+        val API_KEY="25555992fe70125719c4f526b4729330"
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://api.openweathermap.org/data/2.5/forecast?q=${txt_ciudad.text}&appid=${API_KEY}"
+        val jsonRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+              setValues(response)
+            },
+            Response.ErrorListener { Toast.makeText(this,"ERROR",Toast.LENGTH_LONG).show() })
+
+        queue.add(jsonRequest)
+
+    }
+    private fun setValues(response:JSONObject){
+        coordenadas.text=response.getJSONArray("list").getJSONObject(0).getJSONObject("main").getString("temp")
+        //coordenadas.text=response.getJSONObject("city").getString("id")
+
+
+    }
+
 }
