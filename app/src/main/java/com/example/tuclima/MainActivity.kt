f2a968555121d886857984427834d95f86f8ca58
JesusTics <jesusantonio2000@hotmail.com>
2022-08-27 20:04:40 -0600

    Se logro incluir las coordenadas

--- app/src/main/java/com/example/tuclima/MainActivity.kt
+++ app/src/main/java/com/example/tuclima/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.tuclima
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lat=intent.getStringExtra("lat")
+        var long=intent.getStringExtra("long")
+        Toast.makeText(this,lat+" "+long, Toast.LENGTH_LONG).show()
 
     }
 }
